a5eb04d7471f2831c7fa120b9c4dddc370a412d9
zhuowenli <zhuowenligg@gmail.com>
2020-06-01 14:35:41 +0800

    :tada: Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,77 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the {{ object.foo }}. <br>
+      {{msg}}
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <h1>{{ count }}</h1>
+
+    <p v-for="(item, index) in list" :key="index">{{ item.name }}</p>
+
+    <button @click="increase">increase</button>
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import {
+  defineComponent,
+  ref,
+  reactive,
+  onMounted,
+  onUpdated,
+  onUnmounted,
+} from 'vue';
+
+export default defineComponent({
   name: 'HelloWorld',
   props: {
     msg: String,
   },
-};
+  setup(props: any) {
+    const count = ref(0);
+    const object = reactive({ foo: 'bar' });
+    const increase = (): void => {
+      count.value += 1;
+    };
+    const list: Array<object> = [
+      {
+        name: 'foo',
+      },
+      {
+        name: 'bar',
+      },
+      {
+        name: 'coo',
+      },
+    ];
+
+    console.log(props.msg);
+
+    // test('number');
+    // 生命周期
+    onMounted(() => {
+      console.log('onMounted');
+    });
+    onUpdated(() => {
+      console.log('onUpdated');
+    });
+    onUnmounted(() => {
+      console.log('onUnmounted');
+    });
+
+    // 暴露给模板
+    return {
+      count,
+      increase,
+      object,
+      list,
+    };
+  },
+});
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+/*
+ * Author: 卓文理
+ * Email: zhuowenligg@gmail.com
+ * Date: 2020-05-31 10:59:36
+ */
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
